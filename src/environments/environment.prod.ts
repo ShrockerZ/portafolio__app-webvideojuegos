869b1242d73aa6895c4d9c2f1f6e1295a1ee28a9
ShrockerZ <70623520+ShrockerZ@users.noreply.github.com>
2021-06-16 19:14:59 -0600

    update enviroment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_KEY:'key=5bbbbddc6e3148e9b90f4195f2d6cb5c',
+  API_KEY:"",
   API_URL:'https://api.rawg.io/api',
   SERVER: 'http://localhost:4000/api'
 };
